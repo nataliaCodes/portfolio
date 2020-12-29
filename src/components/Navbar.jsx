18a2fa112b24cf07d2f7a0a88f1a26f1594671e5
Natalia Martian <natalia.martian@gmail.com>
2020-12-28 22:21:07 -0500

    Set up components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.scss';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <h1>
+        Buttons go here
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar;
