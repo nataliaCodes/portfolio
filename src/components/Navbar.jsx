8bf87fdd7d70b8b50cc90f65c6658561cd1b3f43
Natalia Martian <natalia.martian@gmail.com>
2020-12-29 23:19:14 -0500

    Style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,36 +3,107 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faBrain, faTerminal, faGraduationCap, faLaptop } from '@fortawesome/free-solid-svg-icons'
 
 import Tooltip from "@material-ui/core/Tooltip";
-import Zoom from '@material-ui/core/Zoom';
+import Fade from '@material-ui/core/Fade';
 
 import './Navbar.scss';
+
 
 function Navbar() {
   return (
     <div className="Navbar">
-      <Tooltip TransitionComponent={Zoom} title="Contact" placement="bottom">
-        <div>
-          <FontAwesomeIcon className="menu-icon envelope" icon={faEnvelope} />
+      <Tooltip 
+        TransitionComponent={Fade} 
+        title="Contact" 
+        placement="top"
+        PopperProps={{
+          popperOptions: {
+            modifiers: {
+              offset: {
+                enabled: true,
+                offset: '10px, -35px',
+              },
+            },
+          },
+        }}
+      >
+        <div className="menu-icon">
+          <FontAwesomeIcon className="envelope" icon={faEnvelope} />
         </div>
       </Tooltip>
-      <Tooltip title="Skills" placement="bottom">
-        <div>
-          <FontAwesomeIcon className="menu-icon" icon={faBrain} />
+      <Tooltip 
+        TransitionComponent={Fade} 
+        title="Skills" 
+        placement="top" 
+        PopperProps={{
+          popperOptions: {
+            modifiers: {
+              offset: {
+                enabled: true,
+                offset: '1px, -35px',
+              },
+            },
+          },
+        }}
+      >
+        <div className="menu-icon">
+          <FontAwesomeIcon className="brain" icon={faBrain} />
         </div>
       </Tooltip>
-      <Tooltip title="Projects" placement="bottom">
-        <div>
-          <FontAwesomeIcon className="menu-icon" icon={faTerminal} />
+      <Tooltip 
+        TransitionComponent={Fade} 
+        title="Projects" 
+        placement="top" 
+        PopperProps={{
+          popperOptions: {
+            modifiers: {
+              offset: {
+                enabled: true,
+                offset: '1px, -35px',
+              },
+            },
+          },
+        }}
+      >
+        <div className="menu-icon">
+          <FontAwesomeIcon className="terminal" icon={faTerminal} />
         </div>
       </Tooltip>
-      <Tooltip title="Education" placement="bottom">
-        <div>
-          <FontAwesomeIcon className="menu-icon cap" icon={faGraduationCap} />
+      <Tooltip 
+        TransitionComponent={Fade} 
+        title="Education" 
+        placement="top" 
+        PopperProps={{
+          popperOptions: {
+            modifiers: {
+              offset: {
+                enabled: true,
+                offset: '1px, -35px',
+              },
+            },
+          },
+        }}
+      >
+        <div className="menu-icon">
+          <FontAwesomeIcon className="cap" icon={faGraduationCap} />
         </div>
       </Tooltip>
-      <Tooltip title="Experience" placement="bottom">
-        <div>
-          <FontAwesomeIcon className="menu-icon" icon={faLaptop} />
+      <Tooltip 
+        TransitionComponent={Fade} 
+        title="Experience" 
+        placement="top" 
+        PopperProps={{
+          popperOptions: {
+            modifiers: {
+              offset: {
+                enabled: true,
+                offset: '1px, -35px',
+              },
+            },
+          },
+        }}
+      >
+        <div className="menu-icon">
+          <FontAwesomeIcon className="laptop" icon={faLaptop} />
         </div>
       </Tooltip>
     </div>
