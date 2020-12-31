047139562942fcb3f21fa09236c6a6d5b4d54a66
Natalia Martian <natalia.martian@gmail.com>
2020-12-31 13:52:12 -0500

    Add soft skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,19 @@
 import './Skills.scss';
 
-// import Skill from './Skill';
 import arrowSkills from '../images/arrow-skills.png'
 
 function Skills() {
   return (
     <div className="Skills">
-      {/* <Skill text="Languages" />
-      <Skill text="Frameworks/libraries" />
-      <Skill text="Testing" />
-      <Skill text="Complementary" /> */}
         <div className="skills-text">
           <h3>Languages</h3>
           <h2>JavaScript, SQL, HTML/CSS</h2>
           <h3>Frameworks/libraries</h3>
-          <h2>NodeJS, Express, ReactJS, Ajax, jQuery, EJS, Bootstrap, SASS, ProgresSQL</h2>
+          <h2 className="move-right">NodeJS, Express, ReactJS, Ajax, jQuery, EJS, Bootstrap, SASS, ProgresSQL</h2>
           <h3>Testing</h3>
-          <h2>Mocha&Chai, Storybook, Jest</h2>
+          <h2 className="move-right">Mocha&Chai, Storybook, Jest</h2>
           <h3>Complementary platforms</h3>
-          <h2>Microsoft Office (Excel, Word, OneNote), Jira, Confluence, SharePoint, PowerApps, Axure, Adobe Suite (Photoshop, Illustrator, After Effects)</h2>
+          <h2 className="move-up">Microsoft Office (Excel, Word, OneNote), Jira, Confluence, SharePoint, PowerApps, Axure, Adobe Suite (Photoshop, Illustrator, After Effects)</h2>
         </div>
         <img src={arrowSkills} alt="graphic-representation-of-skills" />
     </div>
--- src/components/SoftSkills.jsx
+++ src/components/SoftSkills.jsx
@@ -5,7 +5,12 @@ import Languages from './Languages'
 function SoftSkills() {
   return (
     <div className="SoftSkills">
-      <div className="soft-skills">something something</div>
+      <div className="soft-skills">
+        <h1>My skills</h1>
+        <h3>(technical and otherwise)</h3>
+        <p className="soft-skills-list"><span>Agile methodologies,</span> manual testing, <span>bug management,</span> coordination, <span>management</span></p>
+        <p className="soft-skills-list"><span>Team player,</span> attention to detail, <span>problem-solving,</span> time management, <span>pro-active</span></p>
+      </div>
       <Languages />
     </div>
   );
