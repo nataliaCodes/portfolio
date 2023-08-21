d5e1e1728192aa5b41c1cb86463da03c2d54a7bc
Natalia M <natalia.martian@gmail.com>
2023-08-21 15:44:21 -0400

    Remove Axios from project

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,58 +1,6 @@
-import { useState } from "react";
-import axios from 'axios';
+
 
 export default function Contact() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  function onNameChange(e) {
-    console.log('e name:', e.target.value);
-    setName(e.target.value);
-  }
-
-  function onEmailChange(e) {
-    console.log('e email:', e.target.value);
-    setEmail(e.target.value);
-  }
-
-  function onMessageChange(e) {
-    console.log('e message:', e.target.value);
-    setMessage(e.target.value);
-  }
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   console.log('submitevent :', e);
-
-  //   
-  // }
-
-  function resetForm(){
-    setName('');
-    setEmail('');
-    setMessage('');
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(' state:', name, email, message);
-    axios({
-      method: "POST",
-      url:"http://localhost:3002/send",
-      data:  {name, email, message}
-    }).then((response)=>{
-      if (response.data.status === 'success') {
-        console.log("Message Sent.");
-        resetForm();
-      } else if(response.data.status === 'fail') {
-        console.log("Message failed to send.")
-      }
-    }).catch((e) => {
-      console.log('axios e:', e);
-    })
-  }
 
   return (
     <div className="page-content">
