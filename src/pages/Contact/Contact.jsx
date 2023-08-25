8e039e872167c123a793a32945cba930d47a9ba3
Natalia M <natalia.martian@gmail.com>
2023-08-24 20:00:18 -0400

    Add contact form + backend functionality

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,23 +1,32 @@
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const form = useRef();
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+      .then(
+        (result) => {
+          console.log('sent:', result.text);
+          e.target.reset();
+        }, (error) => {
+          console.log('error:', error.text);
+        });
+  };
 
   return (
     <div className="page-content">
-      <form id="contact-form" onSubmit={handleSubmit} method="POST">
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input type="text" className="form-control" value={name} onChange={onNameChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input type="email" className="form-control" aria-describedby="emailHelp" value={email} onChange={onEmailChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea className="form-control" rows="5" value={message} onChange={onMessageChange} />
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+      <form className='flex-column form-container' ref={form} onSubmit={sendEmail}>
+        {/* <label>Name</label> */}
+        <input className='transparent-input' type="text" placeholder='Your name' name="user_name" />
+        {/* <label>Email</label> */}
+        <input className='transparent-input' type="email" placeholder='Your email' name="user_email" />
+        {/* <label>Message</label> */}
+        <textarea className='transparent-input' rows={60} placeholder='Your message' name="message"/>
+        <input type="submit" value="SUBMIT" />
       </form>
     </div>
   );
