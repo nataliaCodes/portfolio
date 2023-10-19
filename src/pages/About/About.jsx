13781b1da8bcdc53749c3df4604a80fd7d2a5e2e
Natalia M <natalia.martian@gmail.com>
2023-10-19 14:38:38 -0400

    Minor fixes

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
         I'm on a perpetual journey of learning and growth, always seeking the next coding challenge to conquer. With a curious mind and an eye for detail, I thrive on turning ideas into reality through the power of technology. Whether I'm crafting intuitive user experiences or diving into the intricacies of algorithms, my goal is to make a meaningful impact in the digital world.
       </p>
       <p className="full-width">
-        Feel free to explore <NavLink exact="true" to="/portfolio" className='underline'>my coding projects</NavLink> and <NavLink exact="true" to="/contact" className='underline'>connect with me</NavLink> on this exciting journey!
+        Feel free to explore <NavLink exact="true" to="/projects" className='underline'>my coding projects</NavLink> and <NavLink exact="true" to="/contact" className='underline'>connect with me</NavLink> on this exciting journey!
       </p>
     </div>
   );
