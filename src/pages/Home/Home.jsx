9fb8fb535b40a15dd1b966cfdf0e9c817384bed6
Natalia M <natalia.martian@gmail.com>
2024-02-22 12:43:56 -0500

    Clean up unused code + adjust media view for small screens

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,4 @@
 import portrait from '../../img/Nat_cartoon_blue.jpg';
-// import { NavLink } from "react-router-dom";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faPersonCircleQuestion, faAddressCard, faBriefcase, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 
@@ -19,60 +16,7 @@ export default function Home() {
         </div>
         <div>Welcome to my site!</div>
       </div>
-        <img className='intro-img' src={portrait} alt="cartoon portrait" />
-      {/* <div className="intro-imgs flex-row">
-        <span className='about'>
-          <NavLink exact="true" to="/about">
-            <FontAwesomeIcon
-              className='intro-svg'
-              icon={faPersonCircleQuestion}
-            ></FontAwesomeIcon>
-            <span className='menu-text flex-row align-center justify-center'>About <br /> me</span>
-          </NavLink>
-        </span>
-        <span className='skills'>
-          <NavLink exact="true" to="/skills">
-            <FontAwesomeIcon
-              className='intro-svg'
-              icon={faAddressCard}
-            ></FontAwesomeIcon>
-            <span className='menu-text flex-row align-center justify-center'>Skills</span>
-          </NavLink>
-        </span>
-        <span className='portfolio'>
-          <NavLink exact="true" to="/projects">
-            <FontAwesomeIcon
-              className='intro-svg'
-              icon={faBriefcase}
-            ></FontAwesomeIcon>
-            <span className='menu-text flex-row align-center justify-center'>Projects</span>
-          </NavLink>
-        </span>
-        <span className='contact'>
-          <NavLink exact="true" to="/contact">
-            <FontAwesomeIcon
-              className='intro-svg'
-              icon={faEnvelope}
-            ></FontAwesomeIcon>
-            <span className='menu-text flex-row align-center justify-center'>Contact</span>
-          </NavLink>
-        </span>
-        <div className='intro-symbol angle'>
-          <p>{'</>'}</p>
-        </div>
-        <div className='intro-symbol curly'>
-          <p>{'{ }'}</p>
-        </div>
-        <div className='intro-symbol square'>
-          <p>{'[...]'}</p>
-        </div>
-        <div className='intro-symbol func'>
-          <p>{'function()'}</p>
-        </div>
-        <div className='intro-symbol arrow'>
-          <p>{'=>'}</p>
-        </div>
-      </div> */}
+      <img className='intro-img' src={portrait} alt="cartoon portrait" />
     </div>
   );
 }
